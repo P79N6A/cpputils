60140708e1dd7c63bbf1aed72709a6f6c4f06c4f
liuyongshuai@hotmail.com <liuyongshuai@hotmail.com>
2019-04-01 15:03:56 +0800

    s

---
+++ src/utils/basic_type.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+ * File:	basic_type.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	Mon 01 Apr 2019 03:03:45 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
